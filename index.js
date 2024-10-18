d9eb4e8bc9278f5f34c76b06ac0dd8334c2d68df
SonyaCode <Sonya.Zh2018@gmail.com>
2024-10-17 22:42:31 -0400

    Commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Clicker App
 let count = 0;
+let entries = document.getElementById("entries")
 
 let displayNum = document.getElementById("count-el")
 displayNum.innerText = count;
@@ -8,4 +9,8 @@ function increment() {
     count++;
     displayNum.innerText = count;
     console.log(count);
+}
+
+function save() {
+    entries.innerHTML += count + " - "
 }
